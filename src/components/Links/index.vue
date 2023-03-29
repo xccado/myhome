036010f11e09ad451a8c8f71cb46c7af4225ec0a
xccado <100438949+xccado@users.noreply.github.com>
2023-03-29 16:32:52 +0800

    Update index.vue

--- src/components/Links/index.vue
+++ src/components/Links/index.vue
@@ -46,32 +46,32 @@ let linksData = [
   {
     icon: Blog,
     name: "博客",
-    link: "https://blog.imsyy.top/",
+    link: "https://blog.qljdv.top/",
   },
   {
     icon: Cloud,
     name: "网盘",
-    link: "https://pan.imsyy.top/",
+    link: "https://pan.qljdv.top/",
   },
   {
     icon: CompactDisc,
     name: "音乐",
-    link: "https://music.imsyy.top/",
+    link: "https://music.qljdv.top/",
   },
   {
     icon: Compass,
     name: "起始页",
-    link: "https://nav.imsyy.top/",
+    link: "https://nav.qljdv.top/",
   },
   {
     icon: Book,
     name: "网址集",
-    link: "https://web.imsyy.top/",
+    link: "https://web.qljdv.top/",
   },
   {
     icon: Fire,
     name: "今日热榜",
-    link: "https://hot.imsyy.top/",
+    link: "https://hot.qljdv.top/",
   },
 ];
 
